9336391918af7132b51117ee159fdd1a8d3817b3
Weihao Liu <weihaoliu99@gmail.com>
2024-01-12 20:42:26 +0100

    carousel update

--- src/components/home/Carousel.js
+++ src/components/home/Carousel.js
@@ -1,12 +1,15 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "../../styles/homepage/wrapper.module.scss";
 import hotelsData from "../../assets/data/hotel-data.json";
 import { UilAngleLeft, UilAngleRight } from "@iconscout/react-unicons";
 
 function Carousel() {
   const itemRefs = useRef([]);
+  let _direction = true; // 轮播方向
 
+  // 切换图片按钮
   const handleNextButton = () => {
+    _direction = true;
     const firstItem = itemRefs.current[0];
     document.querySelector("#slide").appendChild(firstItem);
 
@@ -15,42 +18,76 @@ function Carousel() {
   };
 
   const handlePrevButton = () => {
+    _direction = false;
     const lastItem = itemRefs.current[itemRefs.current.length - 1]; // 获取最后一位
     document.querySelector("#slide").prepend(lastItem); // 添加到第一个
 
     // update ref
     itemRefs.current = [lastItem, ...itemRefs.current.slice(0, -1)];
   };
+
+  // 自动轮播
+
+  let _autoSlide = () => {
+    if (_direction) {
+      let firstItem = itemRefs.current[0];
+      document.querySelector("#slide").appendChild(firstItem);
+
+      // update ref
+      itemRefs.current = [...itemRefs.current.slice(1), firstItem];
+    } else {
+      let lastItem = itemRefs.current[itemRefs.current.length - 1];
+      document.querySelector("#slide").prepend(lastItem);
+
+      // update ref
+      itemRefs.current = [lastItem, ...itemRefs.current.slice(0, -1)];
+    }
+  };
+
+  let timer;
+  useEffect(() => {
+    itemRefs.current = hotelsData.map((_, index) => itemRefs.current[index]); // 进行匹配同步
+    timer = setInterval(_autoSlide, 3000);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <div className={styles.section} id="home">
-      {/* 轮播图 */}
-      <div className={styles.slide} id="slide">
-        {hotelsData.map((hotel, index) => (
-          <div
-            className={styles.item}
-            key={index}
-            style={{ backgroundImage: `url(${hotel.imageUrl})` }}
-            id="item"
-            ref={(el) => (itemRefs.current[index] = el)}
-          >
-            <div className={styles.content}>
-              <div className={styles.name}>{hotel.name}</div>
-              <div className={styles.description}>{hotel.description}</div>
-              <button>See More</button>
+    <div className={styles.container}>
+      <div
+        className={styles.carousel_container}
+        id="home"
+        onMouseOver={() => clearInterval(timer)}
+        onMouseLeave={() => (timer = setInterval(_autoSlide, 3000))}
+      >
+        {/* 轮播图 */}
+        <div className={styles.slide} id="slide">
+          {hotelsData.map((hotel, index) => (
+            <div
+              className={styles.item}
+              key={index}
+              style={{ backgroundImage: `url(${hotel.imageUrl})` }}
+              id="item"
+              ref={(el) => (itemRefs.current[index] = el)}
+            >
+              <div className={styles.content}>
+                <div className={styles.name}>{hotel.name}</div>
+                <div className={styles.description}>{hotel.description}</div>
+                <button>See More</button>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-      {/* 切换图片 */}
-      <div className={styles.buttons}>
-        <div className={styles.s_button}>
-          <UilAngleLeft
-            className={styles.angle}
-            onClick={handlePrevButton}
-          ></UilAngleLeft>
+          ))}
         </div>
-        <div className={styles.s_button} onClick={handleNextButton}>
-          <UilAngleRight className={styles.angle}></UilAngleRight>
+        {/* 切换图片 */}
+        <div className={styles.buttons}>
+          <div className={styles.s_button}>
+            <UilAngleLeft
+              className={styles.angle}
+              onClick={handlePrevButton}
+            ></UilAngleLeft>
+          </div>
+          <div className={styles.s_button} onClick={handleNextButton}>
+            <UilAngleRight className={styles.angle}></UilAngleRight>
+          </div>
         </div>
       </div>
     </div>
